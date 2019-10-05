0f6b55a15d92810639e12c57be760f0a018af63a
WhitePeachTea <penglei2123@gmail.com>
2019-10-05 11:17:03 -0400

    upadte on JS and CSS

--- Script.js
+++ Script.js
@@ -1,12 +1,15 @@
 function GeneratePassword() {
     var password = '';
     var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-+';
-    for (i=128;i>=8;i--) {
-      var i = Math.floor(Math.random()*chars.length + 1);
-      password += chars.charAt(i)
+    for (i=74;i>=0;i--) {
+      var i = Math.floor(Math.random()*chars.length+Math.floor(Math.random()));
+      password = password + chars.charAt(i)
     }
     if (password.length >= 8) {
         return password;
+    }
+    else if (password.length >=128 ){
+        return GeneratePassword();
     }
     else {
         return GeneratePassword();
@@ -14,14 +17,12 @@ function GeneratePassword() {
 };
 
 
-password.length <= 128;
-
 document.getElementById("generate").addEventListener("click", function(){
     document.getElementById("password").innerText = GeneratePassword();});
 
 
-document.getElementById("copy").addEventListener("click", function()
-{var copyText = document.getElementById("password");
+document.getElementById("copy").addEventListener("click", function() {
+    var copyText = document.getElementById("password");
 
     copyText.select();
 
